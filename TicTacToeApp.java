7d38ec38472b1f601a1de556b49935d0be31b9c6
dev-gracy <gracyyyy.g@gmail.com>
2026-05-01 19:22:33 +0530

    UC7: Computer random move

--- TicTacToeApp.java
+++ TicTacToeApp.java
@@ -1,25 +1,43 @@
+import java.util.Random;
+
 public class TicTacToeApp {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
-
-        // initialize board
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
-        }
-
-        // sample move (already validated)
-        placeMove(0, 0, 'X');
-
-        // print updated cell (as shown in PDF)
-        System.out.println(board[0][0]);
+        computerMove();
+        printBoard();
     }
 
-    // UC6 method
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
+    // UC7
+    static void computerMove() {
+        Random rand = new Random();
+
+        while (true) {
+            int slot = rand.nextInt(9) + 1; // 1–9
+
+            int row = (slot - 1) / 3;
+            int col = (slot - 1) % 3;
+
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                break;
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
